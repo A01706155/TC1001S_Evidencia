ddb239622841cf74c048257503e75ad3d9376046
Diego Vega Camacho <A01704492@itesm.mx>
2022-03-25 12:14:09 -0600

    Change in color of x & o

---
+++ tictactoe.py
@@ -0,0 +1,64 @@
+"""Tic Tac Toe
+
+Exercises
+
+1. Give the X and O a different color and width.
+2. What happens when someone taps a taken spot?
+"""
+
+from turtle import *
+
+from freegames import line
+
+
+
+def grid():
+    """Draw tic-tac-toe grid."""
+    line(-67, 200, -67, -200)
+    line(67, 200, 67, -200)
+    line(-200, -67, 200, -67)
+    line(-200, 67, 200, 67)
+
+
+def drawx(x, y):
+    """Draw X player."""
+    line(x,y, x + 80, y + 80)
+    line(x, y + 80, x + 80, y)
+    color('red')
+
+def drawo(x, y):
+    """Draw O player."""
+    up()
+    goto(x + 67, y + 50)
+    down()
+    circle(22)
+    color('blue')
+
+
+def floor(value):
+    """Round value down to grid with square size 133."""
+    return ((value + 200) // 133) * 133 - 200
+
+
+state = {'player': 0}
+players = [drawx, drawo]
+
+
+def tap(x, y):
+    """Draw X or O in tapped square."""
+    x = floor(67)
+    y = floor(50)
+    player = state['player']
+    draw = players[player]
+    draw(x, y)
+    update()
+    state['player'] = not player
+
+
+setup(420, 420, 370, 0)
+hideturtle()
+tracer(False)
+grid()
+update()
+onscreenclick(tap)
+done()
