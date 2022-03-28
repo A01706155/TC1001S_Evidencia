107067c7d24b9a56a73763083eab515958a553a2
Manolo Ramírez Pintor <A01706155@itesm.mx>
2022-03-28 11:48:33 -0600

    Corregir error de parpadeo y posiciones

--- memory.py
+++ memory.py
@@ -8,6 +8,9 @@ Exercises:
 4. Center single-digit tile.
 5. Use letters instead of tiles.
 """
+
+
+# Imports de librerias necesarias
 from random import shuffle
 from turtle import up, goto, down, color, begin_fill, forward,\
     left, end_fill, clear, shape, stamp, write, update,\
@@ -16,16 +19,35 @@ from turtle import up, goto, down, color, begin_fill, forward,\
 
 from freegames import path
 
+
+# Se define la imagen y el tamano
+# del array con el que se jugara
+# ademas, se ponen todos como no
+# marcados y ocultos, se define el
+# contador de taps y la var para
+# dibujar el contador en pantalla.
 car = path('car.gif')
 tiles = list(range(32)) * 2
 state = {'mark': None}
 hide = [True] * 64
 terminado = False
 # Contador de taps
-counter = {'contador': 0}
+counter = {'contador': 1}
 dibujador = Turtle(visible=False)
 
 
+# Con este revisamos si los tiles dentro
+# del rango creado en el inicio coincide
+# y si no pues continuar con la ejecucion.
+def check_tiles():
+    for i in range(32*2):
+        if hide[i]:
+            return False
+    return True
+
+
+# Se dibujan los tiles con color negro
+# a los bordes y blanco por dentro.
 def square(x, y):
     """Draw white square with black outline at (x, y)."""
     up()
@@ -39,27 +61,31 @@ def square(x, y):
     end_fill()
 
 
+# Devuelve las coords del tile a un index.
 def index(x, y):
     """Convert (x, y) coordinates to tiles index."""
     return int((x + 200) // 50 + ((y + 200) // 50) * 8)
 
 
+# Devuelve las coords del tile a un index.
 def xy(count):
     """Convert tiles count to (x, y) coordinates."""
     return (count % 8) * 50 - 200, (count // 8) * 50 - 200
 
 
+# Cuando pulsamos sobre un tile pasa lo siguiente:
 def tap(x, y):
     """Update mark and hidden tiles based on tap."""
     spot = index(x, y)
     mark = state['mark']
 
+    """Aqui dibujamos el contador arriba definiendo
+    lo que estamos guardando, lo imprimimos y le decimos
+    que queremos que sea normal y no negritas"""
     dibujador.undo()
     dibujador.write(counter['contador'], font=('Impact', 18, 'normal'))
     counter['contador'] += 1
-    clear()
     spot = index(x, y)
-    mark = state['mark']
 
     if mark is None or mark == spot or tiles[mark] != tiles[spot]:
         state['mark'] = spot
@@ -72,8 +98,6 @@ def tap(x, y):
 def draw():
     """ Revisa si ya se acabaron los cuadros """
     if check_tiles():
-        dibujador.undo()
-        clear()
         print("Se acabo el juego")
 
     """Draw image and tiles."""
@@ -99,20 +123,12 @@ def draw():
     ontimer(draw, 100)
 
 
-def check_tiles():
-    for i in range(64):
-        if hide[i]:
-            return False
-    return True
-
-
 shuffle(tiles)
 setup(500, 500, 370, 0)
 addshape(car)
 hideturtle()
 tracer(False)
 dibujador.goto(-200, 200)
-dibujador.color('black')
 dibujador.write(counter['contador'], font=('Impact', 18, 'normal'))
 clear()
 listen()
